ddd992c92dbe57ad4fee98d85efe29d92a077903
Hasan Izanloo <izanloo.ha@gmail.com>
2025-04-24 03:01:16 +0330

    pages created

---
+++ src/pages/HomeLayout.jsx
@@ -0,0 +1,5 @@
+function HomeLayout() {
+  return <h1 className="text-4xl">HomeLayout</h1>;
+}
+
+export default HomeLayout;
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,5 @@
+function Landing() {
+  return <h1 className="text-4xl">Landing</h1>;
+}
+
+export default Landing;
