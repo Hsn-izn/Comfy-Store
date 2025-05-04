86f11dbd816f1561e85090535b646033b03c7767
Hasan Izanloo <izanloo.ha@gmail.com>
2025-05-04 06:32:25 +0330

    add loader function to the SinleProduct page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -6,7 +6,6 @@ const url = "/products?featured=true";
 export async function loader() {
   const response = await customFetch(url);
   const products = response.data.data;
-  console.log(response);
   return { products };
 }
 
--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,4 +1,19 @@
+import { useLoaderData } from "react-router-dom";
+import { formatPrice, customFetch } from "../utils";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
+export async function loader({ params }) {
+  const result = await customFetch(`/products/${params.id}`);
+  console.log(result);
+  return { product: result.data.data };
+}
+
 function SingleProduct() {
+  const { product } = useLoaderData();
+  const { image, title, price, description, colors, company } =
+    product.attributes;
+  const dollarsAmount = formatPrice(price);
   return <h1 className="text-4xl">SingleProduct</h1>;
 }
 
