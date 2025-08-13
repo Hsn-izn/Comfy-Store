3d08672d2937bc05584450774cf179d2fe5f00c3
Hasan Izanloo <izanloo.ha@gmail.com>
2025-08-13 20:43:35 +0330

    setup Cart page layout

---
+++ src/components/CartItemsList.jsx
@@ -0,0 +1,9 @@
+function CartItemsList() {
+  return (
+    <div>
+      <h1 className="text-3xl"> CartItemsList</h1>
+    </div>
+  );
+}
+
+export default CartItemsList;
---
+++ src/components/CartTotals.jsx
@@ -0,0 +1,4 @@
+function CartTotals() {
+  return <h1 className="text-3xl">CartTotals</h1>;
+}
+export default CartTotals;
