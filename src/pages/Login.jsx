0a6e0e4eacdd113d3ded64bdb2142de4e2853e98
Hasan Izanloo <izanloo.ha@gmail.com>
2025-08-22 05:17:05 +0330

    setup demo user

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,6 +4,7 @@ import { customFetch } from "../utils";
 import { toast } from "react-toastify";
 import { loginUser } from "../features/user/userSlice";
 import { useDispatch } from "react-redux";
+import { asyncThunkCreator } from "@reduxjs/toolkit";
 
 export const action =
   (store) =>
@@ -26,6 +27,24 @@ export const action =
   };
 
 function Login() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const loginAsGuestUser = async () => {
+    try {
+      const response = await customFetch.post("/auth/local", {
+        identifier: "test@test.com",
+        password: "secret",
+      });
+      dispatch(loginUser(response.data));
+      toast.success("welcome guest user");
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+      toast.error("guest user login error. please try again");
+    }
+  };
+
   return (
     <section className="h-screen grid place-items-center">
       <Form
@@ -33,22 +52,16 @@ function Login() {
         className="card w-96 p-8 bg-base-100 shadow-lg flex flex-col gap-y-4"
       >
         <h4 className="text-center text-3xl font-bold">Login</h4>
-        <FormInput
-          type="email"
-          label="Email"
-          name="identifier"
-          defaultValue="test@test.com"
-        />
-        <FormInput
-          type="password"
-          label="Password"
-          name="password"
-          defaultValue="secret"
-        />
+        <FormInput type="email" label="Email" name="identifier" />
+        <FormInput type="password" label="Password" name="password" />
         <div className="mt-4">
           <SubmitBtn text="login" />
         </div>
-        <button type="button" className="btn btn-secondary btn-block">
+        <button
+          type="button"
+          className="btn btn-secondary btn-block"
+          onClick={loginAsGuestUser}
+        >
           guest user
         </button>
         <p className="text-center">
