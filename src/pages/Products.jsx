23d2b1296b9416d6bfe445395d0ddc544f56cea6
Hasan Izanloo <izanloo.ha@gmail.com>
2025-08-25 12:06:09 +0330

    setup Orders Pagination

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -5,27 +5,27 @@ import { customFetch } from "../utils";
 const url = "/products";
 
 export async function loader({ request }) {
-  // const params = Object.fromEntries([
-  //   ...new URL(request.url).searchParams.entries(),
-  // ]);
-  const params = new URL(request.url).searchParams;
+	// const params = Object.fromEntries([
+	//   ...new URL(request.url).searchParams.entries(),
+	// ]);
+	const params = new URL(request.url).searchParams;
 
-  const response = await customFetch({ url, params });
+	const response = await customFetch({ url, params });
 
-  const products = response.data.data;
-  const meta = response.data.meta;
-  const paramsObject = Object.fromEntries([...params.entries()]);
-  return { products, meta, paramsObject };
+	const products = response.data.data;
+	const meta = response.data.meta;
+	const paramsObject = Object.fromEntries([...params.entries()]);
+	return { products, meta, paramsObject };
 }
 
 function Products() {
-  return (
-    <>
-      <Filters />
-      <ProductsContainer />
-      <PaginationContainer />
-    </>
-  );
+	return (
+		<>
+			<Filters />
+			<ProductsContainer />
+			<PaginationContainer />
+		</>
+	);
 }
 
 export default Products;
